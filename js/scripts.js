d2d279b74377c9a3c00036b41cb3c6008b32f21f
Ben Kiggen and Dan Mace <dan@>
2018-08-13 11:46:48 -0700

    add script

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,56 @@
 $(function () {
-  var array = [];
-  $("button#add").click(function() {
+  $("#sentence-form").submit(function(event){
+    var array1 = $("#sentence-input").val().split(" ");
+    var array2 = [];
 
-    var item = $("input#grocery-input").val();
-    console.log(item);
-    $("#unsorted").append("<li>" + item + "</li>");
-    array.push(item);
-    console.log(array);
-  });
+    array1.forEach(function(word){
+      if(word.length >= 3){
+        array2.push(word);
+      } else {
+      };
+    });
 
-  $("form#grocery-form").submit(function(event) {
+
     event.preventDefault();
-    array.sort();
-    console.log(array);
-    var sortedArray = array.map(function(entry) {
-      return entry.toUpperCase();
-    });
-    console.log(sortedArray);
-    sortedArray.forEach(function(item) {
-      $("#sorted").append("<li>" + item + "</li>");
-    });
-    $("#sorted").show();
-    $(".form-class").hide();
   });
 
 
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var array = [];
+// $("button#add").click(function() {
+//
+//   var item = $("input#grocery-input").val();
+//   console.log(item);
+//   $("#unsorted").append("<li>" + item + "</li>");
+//   array.push(item);
+//   console.log(array);
+// });
+//
+// $("form#grocery-form").submit(function(event) {
+//   event.preventDefault();
+//   array.sort();
+//   console.log(array);
+//   var sortedArray = array.map(function(entry) {
+//     return entry.toUpperCase();
+//   });
+//   console.log(sortedArray);
+//   sortedArray.forEach(function(item) {
+//     $("#sorted").append("<li>" + item + "</li>");
+//   });
+//   $("#sorted").show();
+//   $(".form-class").hide();
+// });
